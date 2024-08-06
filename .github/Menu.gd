141ffe36d3d143e62486730c7900f0ad9c295956
DanteScheffer <18123@rosmini.school.nz>
2024-08-06 14:00:43 +1200

    Buttons
    Coding my buttons to go to other scenes

---
+++ .github/Menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Level 1.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://settings.tscn")
